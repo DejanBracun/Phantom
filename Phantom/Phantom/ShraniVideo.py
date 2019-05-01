3bfd7deb881c60841a33aa3967cb7a47786da5be
DejanBracun <bracun.dejan@gmail.com>
2019-05-01 12:26:41 +0200

    Popravil napako pri shranjevanju videa

--- Phantom/Phantom/ShraniVideo.py
+++ Phantom/Phantom/ShraniVideo.py
@@ -6,9 +6,9 @@ class ShraniVideo:
 	def __init__(self, Ime = "Brez Imena", FPS = 30):
 		self.pot = os.getcwd()
 		st = 0
-		while os.path.isfile(self.pot + "\\\\" + Ime + (str(st) if st else "") + ".avi"):
+		while os.path.isfile(self.pot + "\\" + Ime + (str(st) if st else "") + ".avi"):
 			st += 1
-		self.Ime = Ime + str(st) + ".avi"
+		self.Ime = Ime + (str(st) if st else "") + ".avi"
 		self.FPS = FPS
 		self.out = None
 	
